b859c774e749cc310fdb7b5f8d9d89e21e245277
Partha Aji <paji@redhat.com>
2019-04-29 23:31:16 -0400

    Hangman react game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import randomWord from 'random-words';
+import Char from './components/Char';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    secretWord: randomWord(),
+    filledWord: '',
+    guesses: [],
+    currentGuess: '',
+    guessCounter:0
+  }
+  maxGuesses = 10;
+
+  inputHandler = (evt) => {
+    if(evt.key.match(/^[A-Z]$/i)) {
+      const props = {
+        currentGuess: evt.key,
+        guesses: this.state.guesses.slice() + [evt.key]
+      }
+
+      if(!this.state.secretWord.includes(props.currentGuess)) {
+        props.guessCounter = this.state.guessCounter + 1;
+      }
+
+      this.setState(props);
+    }
+  }
+
+  getFilledWord = () => {
+    let filledWord = [];
+    for(let i = 0; i < this.state.secretWord.length; i++) {
+      if(this.state.guesses.includes(this.state.secretWord[i])) {
+        filledWord.push(this.state.secretWord[i]);
+      }
+      else {
+        filledWord.push("");
+      }
+    }
+    return filledWord;
+  }
+
+  getStatus = () => {
+    const currentGuess = this.state.currentGuess;
+
+    if (this.getFilledWord().join("") === this.state.secretWord) {
+      return (<div> You 've guessed the word yo!! </div>);
+    } else if ( currentGuess !== '' && !this.state.secretWord.includes(currentGuess)){
+      const remainingGuesses = this.maxGuesses - this.state.guessCounter;
+      if(remainingGuesses <= 0) {
+         return(<div> Well you used up your chances. The word was <strong>{this.state.secretWord}</strong> Reload and try again!!</div> );
+      } else {
+        return(<div> Well that ain't here  But you have {remainingGuesses} chances</div> );
+      }
+    }
+  }
+
+  boxWord = () => {
+    return this.getFilledWord().map((character, index) => {
+      return (<Char character={character} key={index} />);
+    });
+  }
+
+  componentWillMount() {
+    document.onkeydown = this.inputHandler;
+  }
+
+  componentWillUnmount() {
+    document.onkeydown = null;
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <p>
+             I have chosen a word with {this.state.secretWord.length} letters. Guess the word.
+          </p>
+          <div>
+              Your Guess <Char character={this.state.currentGuess}/>
+          </div>
+          { this.boxWord() }
+          { this.getStatus() }
+
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
