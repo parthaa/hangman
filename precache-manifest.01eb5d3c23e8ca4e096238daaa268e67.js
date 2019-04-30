4063e981b8bdb59c3c254d95eff0c5ebc4834395
Partha Aji <paji@redhat.com>
2019-04-30 10:36:12 -0400

    Updates

---
+++ precache-manifest.01eb5d3c23e8ca4e096238daaa268e67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "194922e750fb807a0912",
+    "url": "/hangman/static/js/runtime~main.13e36f11.js"
+  },
+  {
+    "revision": "6e460ac21179c647536c",
+    "url": "/hangman/static/js/main.c3c555f9.chunk.js"
+  },
+  {
+    "revision": "561fd9e9e821980f2de2",
+    "url": "/hangman/static/js/2.81806985.chunk.js"
+  },
+  {
+    "revision": "6e460ac21179c647536c",
+    "url": "/hangman/static/css/main.bb7ee5fd.chunk.css"
+  },
+  {
+    "revision": "0883f8682d8e4a1c971647bafb264704",
+    "url": "/hangman/index.html"
+  }
+];
